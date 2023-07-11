d23139e92fa3309fa4816c6ff2d1839cd95afed0
sanghapark234 <is0535xr@ed.ritsumei.ac.jp>
2023-07-11 17:29:42 +0900

    v1

--- indexdy.js
+++ indexdy.js
@@ -1,12 +1,11 @@
 //--for index.html----------
-var time = "";
 
 function submitForm(event) {
     event.preventDefault();
     var charValue = document.getElementById("charInput").value;
     if(charValue == 'A' || charValue == 'a'){
         window.location.href = "1stv1.html";
-    } else if(charValue == 'B'){
+    } else if(charValue == 'B' || charValue == 'b'){
         window.location.href = "1stv2.html";
     } else{
         alert("Enter a valid version");
@@ -16,10 +15,14 @@ function submitForm(event) {
 
 function setFileName() {
     var now = new Date();
-    var h = now.getHours();
-    var m = now.getMinutes();
-    var s = now.getSeconds();
-    time = h.toString()+"_" +m.toString()+ "_"+ s.toString();
+    var year = now.getFullYear();
+    var month = String(now.getMonth() + 1).padStart(2, '0');
+    var day = String(now.getDate()).padStart(2, '0');
+    var hr = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    time = year.toString() + "_" + month.toString()+"_"+day.toString()+"_"+
+    hr.toString()+"_" +min.toString()+ "_"+ sec.toString()+"_A";
     localStorage.setItem("filename", time);
 }
 
