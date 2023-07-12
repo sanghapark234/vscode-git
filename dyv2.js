9a1bbad148c810f8c7b7ea3b7900801286f29ab7
sanghagit <sanghapark123@gmail.com>
2023-07-12 16:35:46 +0900

    v1

--- dyv2.js
+++ dyv2.js
@@ -49,7 +49,7 @@ function createGoBack() {
     goBack.innerHTML = "Go Back";
     goBack.addEventListener("click", function () {
         if (current != 0) {
-            alert("Take " + current);
+            alert(current + "is returned");
         }
         redirectTo1st();
     });
